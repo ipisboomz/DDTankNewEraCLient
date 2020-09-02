96505f9980b2dc5151b9bb21fdcc217833fc22de
Ipis Boomz <ipis.boomz@gmail.com>
2020-09-03 00:27:59 +0800

    Confirm Before Closing

--- DDTLauncher/Form1.Designer.cs
+++ DDTLauncher/Form1.Designer.cs
@@ -55,18 +55,20 @@
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(6, 6);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(614, 452);
+            this.panel1.Size = new System.Drawing.Size(307, 235);
             this.panel1.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label5.Location = new System.Drawing.Point(48, 418);
+            this.label5.Location = new System.Drawing.Point(24, 217);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(229, 26);
+            this.label5.Size = new System.Drawing.Size(109, 13);
             this.label5.TabIndex = 20;
             this.label5.Text = "Press Ctrl + R for ruler";
             // 
@@ -74,9 +76,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(48, 331);
+            this.label4.Location = new System.Drawing.Point(24, 172);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(563, 75);
+            this.label4.Size = new System.Drawing.Size(276, 39);
             this.label4.TabIndex = 19;
             this.label4.Text = "Caution: Download only official build \r\nfrom https://gist.github.com/ipisboomz re" +
     "pository\r\nDo not use custom build as they may steal your password";
@@ -84,9 +87,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(42, 35);
+            this.label3.Location = new System.Drawing.Point(21, 18);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(352, 25);
+            this.label3.Size = new System.Drawing.Size(181, 13);
             this.label3.TabIndex = 18;
             this.label3.Text = "Server in http://ddten.7tgames.com/";
             // 
@@ -99,36 +103,40 @@
             "S1: Lillooet",
             "S2: Emerald Lake",
             "S3: Empire Land"});
-            this.comboBox1.Location = new System.Drawing.Point(47, 74);
+            this.comboBox1.Location = new System.Drawing.Point(24, 38);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(283, 45);
+            this.comboBox1.Size = new System.Drawing.Size(144, 28);
             this.comboBox1.TabIndex = 17;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(42, 229);
+            this.label2.Location = new System.Drawing.Point(21, 119);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 25);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 16;
             this.label2.Text = "Password";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 136);
+            this.label1.Location = new System.Drawing.Point(21, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 25);
+            this.label1.Size = new System.Drawing.Size(91, 13);
             this.label1.TabIndex = 15;
             this.label1.Text = "Username / Email";
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(47, 261);
+            this.textBox2.Location = new System.Drawing.Point(24, 136);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(283, 44);
+            this.textBox2.Size = new System.Drawing.Size(144, 26);
             this.textBox2.TabIndex = 14;
             // 
             // textBox1
@@ -136,18 +144,20 @@
             this.textBox1.AutoCompleteMode = global::DDTLauncher.Properties.Settings.Default.UsernameOrEmail;
             this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("AutoCompleteMode", global::DDTLauncher.Properties.Settings.Default, "UsernameOrEmail", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(47, 168);
+            this.textBox1.Location = new System.Drawing.Point(24, 87);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(283, 44);
+            this.textBox1.Size = new System.Drawing.Size(144, 26);
             this.textBox1.TabIndex = 13;
             // 
             // button1
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(366, 168);
+            this.button1.Location = new System.Drawing.Point(183, 87);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(228, 137);
+            this.button1.Size = new System.Drawing.Size(114, 71);
             this.button1.TabIndex = 12;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
@@ -158,9 +168,10 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 470);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 244);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(638, 40);
+            this.pictureBox1.Size = new System.Drawing.Size(319, 21);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
@@ -168,19 +179,21 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(638, 510);
+            this.ClientSize = new System.Drawing.Size(319, 265);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Location = new System.Drawing.Point(0, 0);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.RightToLeftLayout = true;
-            this.Text = "DDTank New Era Launcher  1.1";
+            this.Text = "DDTank New Era Launcher  1.1.1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
--- DDTLauncher/Form1.cs
+++ DDTLauncher/Form1.cs
@@ -192,5 +192,13 @@ namespace DDTLauncher
                 }
             }
         }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Do you want to close this application?", "Exit", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
     }
 }
