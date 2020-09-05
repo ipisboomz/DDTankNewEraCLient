1d5c3eab67ce33f05e786108eec94472f7006cf4
Ipis Boomz <ipis.boomz@gmail.com>
2020-09-05 22:42:55 +0800

    Advanced Controls

--- DDTLauncher/Form1.Designer.cs
+++ DDTLauncher/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -40,12 +41,15 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.comboBox3);
+            this.panel1.Controls.Add(this.comboBox2);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label3);
@@ -55,88 +59,109 @@
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(6, 6);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(317, 235);
+            this.panel1.Size = new System.Drawing.Size(751, 544);
             this.panel1.TabIndex = 7;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Size",
+            "50%",
+            "75%",
+            "100%",
+            "125%",
+            "150%",
+            "200%"});
+            this.comboBox2.Location = new System.Drawing.Point(366, 73);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(141, 37);
+            this.comboBox2.TabIndex = 21;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.label5.Location = new System.Drawing.Point(24, 217);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 9F);
+            this.label5.Location = new System.Drawing.Point(44, 336);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(109, 13);
+            this.label5.Size = new System.Drawing.Size(318, 192);
             this.label5.TabIndex = 20;
-            this.label5.Text = "Press Ctrl + R for ruler";
+            this.label5.Text = "Shortcuts:\r\n\r\nCtrl + R  = Ruler\r\nCtrl + B  = Borderless\r\nCtrl + F5 = Reload\r\nCtrl" +
+    " + +  = Zoom In\r\nCtrl + -  = Zoom Out\r\nCtrl + P  = Screenshot";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.ForeColor = System.Drawing.Color.Red;
-            this.label4.Location = new System.Drawing.Point(24, 172);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Location = new System.Drawing.Point(412, 378);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(276, 39);
+            this.label4.Size = new System.Drawing.Size(326, 150);
             this.label4.TabIndex = 19;
-            this.label4.Text = "Caution: Download only official build \r\nfrom https://gist.github.com/ipisboomz re" +
-    "pository\r\nDo not use custom build as they may steal your password";
+            this.label4.Text = "Caution:\r\nDo not give password to anyone \r\nDownload only from \r\nhttps://github.co" +
+    "m/ipisboomz\r\n\r\nNot For Sale";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(21, 18);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(42, 35);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(181, 13);
+            this.label3.Size = new System.Drawing.Size(352, 25);
             this.label3.TabIndex = 18;
             this.label3.Text = "Server in http://ddten.7tgames.com/";
             // 
             // comboBox1
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "S1: Lillooet",
             "S2: Emerald Lake",
             "S3: Empire Land"});
-            this.comboBox1.Location = new System.Drawing.Point(24, 38);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Location = new System.Drawing.Point(48, 73);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(144, 28);
+            this.comboBox1.Size = new System.Drawing.Size(284, 37);
             this.comboBox1.TabIndex = 17;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(21, 119);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(42, 229);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.Size = new System.Drawing.Size(106, 25);
             this.label2.TabIndex = 16;
             this.label2.Text = "Password";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 71);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(42, 137);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.Size = new System.Drawing.Size(181, 25);
             this.label1.TabIndex = 15;
             this.label1.Text = "Username / Email";
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(24, 136);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Location = new System.Drawing.Point(48, 262);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(144, 26);
+            this.textBox2.Size = new System.Drawing.Size(284, 44);
             this.textBox2.TabIndex = 14;
             // 
             // textBox1
@@ -144,20 +169,20 @@
             this.textBox1.AutoCompleteMode = global::DDTLauncher.Properties.Settings.Default.UsernameOrEmail;
             this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("AutoCompleteMode", global::DDTLauncher.Properties.Settings.Default, "UsernameOrEmail", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(24, 87);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Location = new System.Drawing.Point(48, 167);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(144, 26);
+            this.textBox1.Size = new System.Drawing.Size(284, 44);
             this.textBox1.TabIndex = 13;
             // 
             // button1
             // 
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(183, 87);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(366, 167);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 71);
+            this.button1.Size = new System.Drawing.Size(324, 137);
             this.button1.TabIndex = 12;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
@@ -168,31 +193,47 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = null;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 244);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 554);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(329, 21);
+            this.pictureBox1.Size = new System.Drawing.Size(772, 40);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Visible = false;
             // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Quality",
+            "Low",
+            "High"});
+            this.comboBox3.Location = new System.Drawing.Point(515, 73);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(175, 37);
+            this.comboBox3.TabIndex = 22;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(329, 265);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(192F, 192F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(772, 594);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.panel1);
+            this.EscapeKeyToClose = false;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.Location = new System.Drawing.Point(0, 0);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.RightToLeftLayout = true;
-            this.Text = "DDTank New Era Launcher  1.2.0";
+            this.Text = "DDTank New Era Launcher  1.3.0";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.panel1.ResumeLayout(false);
@@ -215,6 +256,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
     }
 }
 
--- DDTLauncher/Form1.cs
+++ DDTLauncher/Form1.cs
@@ -11,21 +11,30 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Web;
 using unvell.D2DLib.WinForm;
+using System.Drawing.Imaging;
 
 namespace DDTLauncher
 {
     public partial class Form1 : D2DForm
     {
+        private string protocol = "http";
         private Boolean withRuler = false;
         private Boolean playing = false;
+        private string session;
+        private int server;
+        private int quality;
+        private int zoom = 100;
+        private AxShockwaveFlashObjects.AxShockwaveFlash axFlash;
         private CookieContainer cookieContainer = new CookieContainer();
         public Form1()
         {
             InitializeComponent();
             comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
         }
 
-        private string getSwfUrl(string url)
+        private string GetSwfUrl(string url)
         {
             string swf = "";
 
@@ -55,32 +64,34 @@ namespace DDTLauncher
             return swf;
         }
 
-        private void loadSwf(string swfUrl, int server)
+        private void LoadSwf()
         {
-            AxShockwaveFlashObjects.AxShockwaveFlash axFlash;
+            if (playing)
+            {
+                this.Controls.Remove(axFlash);
+                axFlash.Dispose();
+            }
+
             axFlash = new AxShockwaveFlashObjects.AxShockwaveFlash();
             axFlash.BeginInit();
-            this.SuspendLayout();
             axFlash.Location = new Point(0, 0);
             axFlash.Name = "Main";
             axFlash.TabIndex = 0;
-            axFlash.Size = new Size(1000, 600);
-            this.ClientSize = new Size(1000, 600);
+            ResizeGameWindow();
             axFlash.EndInit();
-            this.ResumeLayout(false);
 
             this.Controls.Add(axFlash);
             axFlash.WMode = "Direct";
-            axFlash.ScaleMode = 0;
-            // axFlash.Quality = 0; // Set low quality for low-end pc
-            axFlash.LoadMovie(0, "http://s" + server + "-ddt.7tgames.com//" + this.getSwfUrl(swfUrl));
+            // axFlash.ScaleMode = 1;
+            axFlash.SetVariable("quality", "Medium");
+            axFlash.Quality = quality;
+            axFlash.LoadMovie(0, protocol + "://s" + server + "-ddt.7tgames.com//" + GetSwfUrl(session));
             axFlash.DisableLocalSecurity();
         }
 
-        private string getServerSession(string cookie, int server)
+        private void SetSession()
         {
-            string session = "";
-            var url = "http://ddten.7tgames.com/playgame/s" + server;
+            var url = protocol + "://ddten.7tgames.com/playgame/s" + server;
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.CookieContainer = cookieContainer;
@@ -102,18 +113,18 @@ namespace DDTLauncher
                 int iframeIndex = data.IndexOf(iframeStart) + iframeStart.Length;
                 string left = data.Substring(iframeIndex, data.Length - iframeIndex);
                 iframeIndex = left.IndexOf("\"");
+
                 session = left.Substring(0, iframeIndex);
 
                 response.Close();
                 readStream.Close();
             }
-            return session;
-
         }
-        private Boolean login(string username, string password, int server)
+        private Boolean Login(string username, string password, int serverIndex)
         {
+            server = serverIndex;
             var success = false;
-            var loginFormUrl = "http://7tgames.com/SignIn?ReturnUrl=http%3A%2F%2Fddten.7tgames.com/playgame/s2";
+            var loginFormUrl = protocol + "://7tgames.com/SignIn?ReturnUrl=http%3A%2F%2Fddten.7tgames.com/playgame/s2";
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(loginFormUrl);
 
@@ -127,7 +138,7 @@ namespace DDTLauncher
             request.Method = "POST";
             request.KeepAlive = true;
             request.Headers.Add("Keep-Alive: 300");
-            request.Referer = "http://ddten.7tgames.com";
+            request.Referer = protocol + "://ddten.7tgames.com";
             request.ContentType = "application/x-www-form-urlencoded";
             byte[] body = Encoding.ASCII.GetBytes(postData);
             request.ContentLength = body.Length;
@@ -156,10 +167,17 @@ namespace DDTLauncher
 
                 if (data.IndexOf("\"success\"") > 0)
                 {
-
-                    string session = getServerSession(response.Headers["Set-Cookie"], server);
-
-                    this.loadSwf(session, server);
+                    Application.DoEvents();
+                    SetSession();
+                    Application.DoEvents();
+                    if (!string.IsNullOrEmpty(session))
+                    {
+                        LoadSwf();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Unable to load session!");
+                    }
 
                     success = true;
                 }
@@ -171,10 +189,10 @@ namespace DDTLauncher
 
         private void button1_Click(object sender, EventArgs e)
         {
+            quality = comboBox3.SelectedIndex == 0 ? 1 : comboBox3.SelectedIndex - 1;
             panel1.Enabled = false;
-            if (!this.login(textBox1.Text, textBox2.Text, comboBox1.SelectedIndex + 1))
+            if (!Login(textBox1.Text, textBox2.Text, comboBox1.SelectedIndex + 1))
             {
-                panel1.Show();
                 panel1.Enabled = true;
                 MessageBox.Show("Unable to login, please check your credentials!");
             }
@@ -187,25 +205,116 @@ namespace DDTLauncher
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            if (!playing)
+            {
+                return;
+            }
             if (e.KeyData == (Keys.Control | Keys.R))
             {
+                e.Handled = true;
                 withRuler = !withRuler;
-                if (playing)
+                pictureBox1.Visible = withRuler;
+                ResizeGameWindow();
+            }
+            else if (e.KeyData == (Keys.Control | Keys.F5))
+            {
+                e.Handled = true;
+                if (MessageBox.Show("Do you want reload?", "Reload Game", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
                 {
-                    pictureBox1.Height = 28;
-                    pictureBox1.Visible = withRuler;
-                    this.ClientSize = withRuler ? new Size(1000, 628) : new Size(1000, 600);
-                    this.BackColor = Color.Black;
+                    SetSession();
+                    if (!string.IsNullOrEmpty(session))
+                    {
+                        LoadSwf();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Unable to load session!");
+                    }
+
                 }
             }
+            else if (e.KeyData == (Keys.Control | Keys.D0))
+            {
+                e.Handled = true;
+                zoom = 100;
+                ResizeGameWindow();
+            }
+            else if (e.KeyData == (Keys.Control | Keys.Oemplus))
+            {
+                e.Handled = true;
+                zoom = Math.Min(250, zoom + 25);
+                ResizeGameWindow();
+            }
+            else if (e.KeyData == (Keys.Control | Keys.OemMinus))
+            {
+                e.Handled = true;
+                zoom = Math.Max(25, zoom - 25);
+                ResizeGameWindow();
+            }
+            else if (e.KeyData == (Keys.Control | Keys.B))
+            {
+                e.Handled = true;
+                this.FormBorderStyle = this.FormBorderStyle == FormBorderStyle.None ? FormBorderStyle.FixedSingle : FormBorderStyle.None;
+                ResizeGameWindow();
+            }
+            else if (e.KeyData == (Keys.Control | Keys.P))
+            {
+                e.Handled = true;
+                this.TakeScreenShot();
+            }
+        }
+
+        private void ResizeGameWindow()
+        {
+            double size = zoom / 100.0;
+            int w = Convert.ToInt32(1000 * size);
+            int h = Convert.ToInt32(600 * size);
+            int rulerHeight = withRuler ? Convert.ToInt32(28 * size) : 0;
+            if (withRuler)
+            {
+                pictureBox1.Height = rulerHeight;
+            }
+            axFlash.Size = new Size(w, h);
+            this.ClientSize = new Size(w, h + rulerHeight);
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (MessageBox.Show("Do you want to close this application?", "Exit", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
+            if (playing && MessageBox.Show("Do you want to close this application?", "Exit", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
             {
                 e.Cancel = true;
             }
         }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox2.SelectedIndex == 0)
+            {
+                zoom = 100;
+            }
+            else
+            {
+                zoom = (comboBox2.SelectedIndex + 1) * 25;
+            }
+        }
+        private void TakeScreenShot()
+        {
+            Point p = axFlash.PointToScreen(axFlash.Location);
+            Bitmap tmpImg = new Bitmap(axFlash.Width, axFlash.Height);
+            using (Graphics g = Graphics.FromImage(tmpImg))
+            {
+                g.CopyFromScreen(p, Point.Empty, axFlash.Size);
+            }
+
+            string DirPath = "ScreenShots";
+            if (!System.IO.Directory.Exists(DirPath))
+            {
+                System.IO.Directory.CreateDirectory(DirPath);
+            }
+            var filename = DirPath + "\\" + DateTime.Now.ToString("yyyy-MM-dd hh-mm-ss-fff") + ".jpg";
+            tmpImg.Save(filename, ImageFormat.Jpeg);
+        }
+
+
     }
 }
